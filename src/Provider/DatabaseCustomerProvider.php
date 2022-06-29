91174dca7437480807e0217ee77346c1cf43eb35
Bram Van der Sype <bram.vandersype@gmail.com>
2022-06-29 15:00:05 +0200

    Introduce interfaces for providers

---
+++ src/Provider/DatabaseCustomerProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brammm\TestingWorkshop\Provider;
+
+use Brammm\TestingWorkshop\Model\Customer;
+use DateTimeImmutable;
+use Doctrine\DBAL\Connection;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+final class DatabaseCustomerProvider implements CustomerProvider
+{
+    public function __construct(
+        private readonly Connection $connection
+    ) {
+    }
+
+    public function findById(UuidInterface $id): Customer
+    {
+        return $this->hydrate(
+            $this->connection->fetchAllAssociative('SELECT * FROM customer WHERE id = ?', [$id->toString()])
+        );
+    }
+
+    /**
+     * @return Customer[]
+     */
+    public function findAll(): array
+    {
+        return array_map(
+            [$this, 'hydrate'],
+            $this->connection->fetchAllAssociative('SELECT * FROM customer ORDER BY name')
+        );
+    }
+
+    /**
+     * @param array{id: string, name: string, customer_since: string} $row
+     */
+    private function hydrate(array $row): Customer
+    {
+        return new Customer(
+            Uuid::fromString($row['id']),
+            $row['name'],
+            new DateTimeImmutable($row['customer_since'])
+        );
+    }
+}
--- src/container.php
+++ src/container.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 use Brammm\TestingWorkshop\Discount\Calculator;
 use Brammm\TestingWorkshop\Discount\MoreThanFiftyDiscount;
+use Brammm\TestingWorkshop\Provider\CustomerProvider;
+use Brammm\TestingWorkshop\Provider\DatabaseCustomerProvider;
+use Brammm\TestingWorkshop\Provider\DatabaseOrderProvider;
+use Brammm\TestingWorkshop\Provider\OrderProvider;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
 use Money\Currencies\ISOCurrencies;
@@ -19,9 +23,12 @@ return [
         'driver' => 'pdo_mysql',
     ]),
 
-    Calculator::class => fn() => new Calculator(
+    Calculator::class => fn () => new Calculator(
         new MoreThanFiftyDiscount(),
     ),
+
+    CustomerProvider::class => fn (Connection $conn) => new DatabaseCustomerProvider($conn),
+    OrderProvider::class => fn (Connection $conn) => new DatabaseOrderProvider($conn),
 
     MoneyFormatter::class => fn () => new IntlMoneyFormatter(
         new NumberFormatter('nl_BE', NumberFormatter::CURRENCY),
